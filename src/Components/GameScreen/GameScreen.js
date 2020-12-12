3a3636287d8d0035ca5da41fd228f8506b1c2414
issamBoutissante <boutissante.issam.dev@gmail.com>
2020-12-12 17:14:06 +0100

    Yeah I solved the problem

--- src/Components/GameScreen/GameScreen.js
+++ src/Components/GameScreen/GameScreen.js
@@ -4,11 +4,13 @@ import { InfoContext } from "../InfoContext/InfoContext";
 import Game from "./Game/Game";
 
 export default function GameScreen(props) {
+  console.log("rendered again");
   const { Socket, RoomId, HosterName, FriendName } = useContext(InfoContext);
   const [finalScore, setFinalScore] = useState(15);
   const [player1] = useState(HosterName);
   const [player2] = useState(FriendName);
   const [randomNumber, setRandomNumber] = useState(0);
+  //const currentPlayer = useRef(HosterName);
   const [currentPlayer, setCurrentPlayer] = useState(HosterName);
   const [player1Total, setPlayer1Total] = useState(0);
   const [player2Total, setPlayer2Total] = useState(0);
@@ -16,57 +18,13 @@ export default function GameScreen(props) {
   const [player2Score, setPlayer2Score] = useState(0);
   const player1StyleRef = useRef(null);
   const player2StyleRef = useRef(null);
-  //here we initialize our listeners to the server
   useEffect(() => {
-    Socket.on("DiceRolled", ({ ranNum }) => {
-      setRandomNumber(ranNum);
-      if (ranNum === 1) {
-        //Here we have to add a sound for losing score
-        //and vibre the cube
-        console.log("the guessed number was 1");
-        TogglePlayer();
-        setPlayer1Score(0);
-        setPlayer2Score(0);
-      } else {
-        if (currentPlayer == player1) {
-          setPlayer1Score((prev) => prev + ranNum);
-          console.log("added to player 1 score ");
-        } else if (currentPlayer == player2) {
-          setPlayer2Score((prev) => prev + ranNum);
-          console.log("added to player 2 score");
-        }
-      }
-    });
-    Socket.on("ScoreHolded", ({ score }) => {
-      console.log("score holded");
-      if (currentPlayer === player1) {
-        setPlayer1Total((prev) => prev + score);
-      } else if (currentPlayer === player2) {
-        setPlayer2Total((prev) => prev + score);
-      }
-      TogglePlayer();
-      setPlayer1Score(0);
-      console.log("score of player1 is " + player1Score);
-      setPlayer2Score(0);
-      console.log("score of player2 is " + player2Score);
-    });
-  }, []);
+    console.log(currentPlayer);
+  });
+  //here we initialize our listeners to the server
+  useEffect(() => {}, []);
   //this player will toggle players and their style
-  const TogglePlayer = () => {
-    console.log("the player has beeeeen toggled");
-    if (currentPlayer === player1) {
-      setCurrentPlayer(player2);
-      player2StyleRef.current.classList.add("activePlayer");
-      player1StyleRef.current.classList.remove("activePlayer");
-    } else if (currentPlayer == player2) {
-      setCurrentPlayer(player1);
-      player1StyleRef.current.classList.add("activePlayer");
-      player2StyleRef.current.classList.remove("activePlayer");
-    }
-  };
-  const onRollDiceHandler = () => {
-    Socket.emit("RollDice", { roomId: RoomId });
-  };
+
   const CheckWinner = (winner) => {
     if (player1Total >= finalScore) {
       player1StyleRef.current.classList.add("winer");
@@ -74,30 +32,10 @@ export default function GameScreen(props) {
       player2StyleRef.current.classList.add("winer");
     }
   };
+
   useEffect(() => {
     CheckWinner();
-
-    console.log(`The hoster is ${HosterName}`);
-    console.log(`The Friend is ${FriendName}`);
-    console.log(`The current Player is ${currentPlayer}`);
   }, [player1Total, player2Total]);
-  const onHoldHandler = () => {
-    console.log("==================================");
-    console.log(`'Hold' player1 score is ${player1Score}`);
-    console.log(`'Hold' player2 score is ${player2Score}`);
-    if (player1Score > 0) {
-      Socket.emit("Hold", { roomId: RoomId, score: player1Score });
-      console.log(`after test 'Hold' player1 score is ${player1Score}`);
-    } else {
-      Socket.emit("Hold", { roomId: RoomId, score: player2Score });
-      console.log(`after test 'Hold' player2 score is ${player2Score}`);
-    }
-    // if (currentPlayer === player1) {
-
-    // } else {
-
-    // }
-  };
 
   return (
     <Game
--- src/Components/OnlinePlay/OnlinePlay.js
+++ src/Components/OnlinePlay/OnlinePlay.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, NavLink } from "react-router-dom";
 import StartNewGame from "./StartNewGame/StartNewGame";
 import JoinGame from "./JoinGame/JoinGame";
 import GameScreen from "../GameScreen/GameScreen";
-import TestGame from "../GameScreen/testGame";
+import GameScreenTwo from "../GameScreen/GameScreen2";
 export default function OnlinePlay() {
   return (
     <div>
@@ -16,7 +16,7 @@ export default function OnlinePlay() {
         </Route>
         <Route path="/StartNewGame" exact component={StartNewGame}></Route>
         <Route path="/JoinGame" exact component={JoinGame}></Route>
-        <Route path="/GameScreen" exact component={GameScreen}></Route>
+        <Route path="/GameScreen" exact component={GameScreenTwo}></Route>
       </BrowserRouter>
     </div>
   );
